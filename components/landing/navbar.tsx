4224e8ef8cb9ddae49ab69165a4da588b2043bda
TheoBIET <dev.theobiet@gmail.com>
2022-06-11 17:07:16 +0200

    🔧 | Phantom Wallet Connection OK

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -1,37 +1,43 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../../styles/components/Navbar.module.scss";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useEffect } from "react";
 
 export default function Navbar() {
+  const { publicKey, wallet, disconnect } = useWallet();
+
+  useEffect(() => {
+    if (wallet && publicKey) {
+      
+    }
+  }, [wallet, publicKey]);
+
   return (
     <div className={styles.container}>
-        <div className={styles.logo}>
-          <Link href="/">
-            <a>
-              <Image
-                src="/logo_white.png"
-                alt="logo"
-                height={120}
-                width={120}
-              />
-            </a>
-          </Link>
-        </div>
-        <nav className={styles.nav}>
-          <Link href="/">
-            <a className={styles.link}>Home</a>
-          </Link>
-          <Link href="/roadmap">
-            <a className={styles.link}>Roadmap</a>
-              </Link>
-              <Link href="/roadmap">
-            <a className={styles.link}>Team</a>
-          </Link>
-          <Link href="https://discord.gg/e8BDZXpGfS">
-            <a className={styles.link}>Discord</a>
-          </Link>
-        </nav>
-      <div className={styles.button}>Connect Wallet</div>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>
+            <Image src="/logo_white.png" alt="logo" height={120} width={120} />
+          </a>
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link>
+        <Link href="/roadmap">
+          <a className={styles.link}>Roadmap</a>
+        </Link>
+        <Link href="/roadmap">
+          <a className={styles.link}>Team</a>
+        </Link>
+        <Link href="https://discord.gg/e8BDZXpGfS">
+          <a className={styles.link}>Discord</a>
+        </Link>
+      </nav>
+      <WalletMultiButton />
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,67 @@
 import "../styles/index.scss";
-import type { AppProps } from "next/app";
-import type { NextPage } from 'next'
-import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from "next";
+import type { ReactElement, ReactNode } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { AppProps } from "next/app";
+import { FC, useMemo } from "react";
+
+// Use require instead of import since order matters
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/index.scss");
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(<Component {...pageProps} />)
-}
+const App: FC<AppProps> = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-export default MyApp;
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
